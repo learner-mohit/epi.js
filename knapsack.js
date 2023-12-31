e93bab33bbe6f2c40b6046368e8a8e1bc7573539
Mohit Puri <mohit.puri@paytmpayments.com>
2023-12-31 20:10:00 +0530

    knapsack

--- knapsack.js
+++ knapsack.js
@@ -5,36 +5,35 @@ class Item {
   }
 }
 
-function optimumSubjectToCapacity(items, capacity) {
+const solve = (items, capacity) => {
   const cache = Array.from({ length: items.length }, () =>
     Array.from({ length: capacity + 1 }, () => -1)
   );
   return helper(0, capacity, items, cache);
-}
+};
 
-function helper(idx, capacity, items, cache) {
-  if (idx === items.length) {
+const helper = (i, capacity, items, cache) => {
+  if (i === items.length) {
     return 0;
   }
-  if (cache[idx][capacity] === -1) {
-    const weight = items[idx].weight;
-    const value = items[idx].value;
+  if (cache[i][capacity] === -1) {
+    const { weight, value } = items[i];
+    const valueExcluded = helper(i + 1, capacity, items, cache);
     const valueIncluded =
       capacity - weight >= 0
-        ? value + helper(idx + 1, capacity - weight, items, cache)
+        ? value + helper(i + 1, capacity - weight, items, cache)
         : 0;
-    const valueExcluded = helper(idx + 1, capacity, items, cache);
-    cache[idx][capacity] = Math.max(valueIncluded, valueExcluded);
+    cache[i][capacity] = Math.max(valueExcluded, valueIncluded);
   }
-  return cache[idx][capacity];
-}
+  return cache[i][capacity];
+};
 
-// Test the function
 const items = [
   new Item(5, 60),
   new Item(3, 50),
   new Item(4, 70),
   new Item(2, 30),
 ];
+
 const capacity = 5;
-console.log(optimumSubjectToCapacity(items, capacity)); // Outputs the maximum value that can be obtained
+console.log(solve(items, capacity));
