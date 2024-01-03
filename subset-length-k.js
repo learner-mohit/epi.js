a0312d066f8015653bbdf55e9337b992aec0a605
Mohit Puri <mohit.puri@paytmpayments.com>
2024-01-03 07:51:19 +0530

    subset-length-k.js

--- subset-length-k.js
+++ subset-length-k.js
@@ -1,19 +1,20 @@
 const solve = (arr, k) => {
   const result = [];
-  helper(0, [], arr, result, k);
+  helper(0, [], k, arr, result);
   return result;
 };
 
-const helper = (i, subset, arr, result, k) => {
+const helper = (i, subset, k, arr, result) => {
   if (subset.length === k) {
     result.push(subset);
     return;
   }
-  if (i === arr.length) {
-    return;
+  for (let j = i; j < arr.length; j++) {
+    helper(j + 1, subset.concat(arr[j]), k, arr, result);
   }
-  helper(i + 1, subset.concat(arr[i]), arr, result, k);
-  helper(i + 1, subset, arr, result, k);
 };
-
+console.log(solve(["a", "b", "c"], 3));
 console.log(solve(["a", "b", "c"], 2));
+
+// TC: O(k*C(n, k))
+// SC: O(k*C(n, k))
