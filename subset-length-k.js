e52060520beea00d4dc01c4115b84ec200172581
Mohit Puri <mohit.puri@paytmpayments.com>
2024-01-09 10:19:46 +0530

    solved

--- subset-length-k.js
+++ subset-length-k.js
@@ -1,18 +1,20 @@
-const solve = (A, k) => {
+const solve = (arr, k) => {
   const result = [];
-  helper(0, [], k, A, result);
+  helper(0, [], k, arr, result);
   return result;
 };
-const helper = (i, subset, k, A, result) => {
+
+const helper = (i, subset, k, arr, result) => {
   if (subset.length === k) {
     result.push(subset);
     return;
   }
-  if (i === A.length) {
-    return;
+  for (let j = i; j < arr.length; j++) {
+    helper(j + 1, subset.concat(arr[j]), k, arr, result);
   }
-  helper(i + 1, subset.concat(A[i]), k, A, result);
-  helper(i + 1, subset, k, A, result);
 };
 console.log(solve(["a", "b", "c"], 3));
 console.log(solve(["a", "b", "c"], 2));
+
+// TC: O(k*C(n, k))
+// SC: O(k*C(n, k))
